4ed42809591fcd3890f1b285ef06459140d67168
Septiandi Nugraha <septiandin92@gmail.com>
2023-08-16 19:41:33 +0700

    Menambahkan code palindrome

--- 02_Basic-Programming/praktikum/soal_eksplorasi/se_1_palindrome.go
+++ 02_Basic-Programming/praktikum/soal_eksplorasi/se_1_palindrome.go
@@ -1,2 +1,34 @@
 // soal nomor 1
-//
+// Buatlah sebuah program untuk memeriksa apakah sebuah kata adalah palindrome atau bukan, serta coba terapkan scanner untuk menangkap inputan dari console, seperti dibawah ini!
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+func main() {
+	fmt.Print("Program pengecekan kata Palindrom \n")
+	var kata string
+	fmt.Print("masukkan sebuah kata: ")
+	fmt.Scan(&kata)
+
+	if palindrome(kata) {
+		fmt.Printf("%s merupakan palindrome.\n", kata)
+	} else {
+		fmt.Printf("%s tidak termasuk palindrome.\n", kata)
+	}
+}
+
+func palindrome(str string) bool {
+	str = strings.ToLower(str)
+	length := len(str)
+	for i := 0; i < length/2; i++ {
+		if str[i] != str[length-1-i] {
+			return false
+		}
+	}
+	return true
+}
