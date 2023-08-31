970f06ee96d5f8a379dd5d3d321804a1e423536f
Septiandi Nugraha <septiandin92@gmail.com>
2023-08-31 19:43:52 +0700

    modify soal prioritas 2 materi 07

--- 07_Recursive-Number-Theory-Sorting-&-Searching/praktikum/soal_prioritas_2/nomor_1/p2_1_playingdomino.go
+++ 07_Recursive-Number-Theory-Sorting-&-Searching/praktikum/soal_prioritas_2/nomor_1/p2_1_playingdomino.go
@@ -9,16 +9,12 @@ import "fmt"
 
 func playingDomino(cards [][]int, deck []int) interface{} {
 	for _, card := range cards {
-		if card[0] == deck[0] ||
-		   card[1] == deck[0] ||
-		   card[0] == deck[1] ||
-		   card[1] == deck[1] {
+		if card[0] == deck[0] || card[1] == deck[0] || card[0] == deck[1] || card[1] == deck[1] {
 			return card
 		}
 	}
 	return "Tutup kartu"
 }
-
 
 func main() {
 	fmt.Println(playingDomino([][]int{[]int{6, 5}, []int{3, 4}, []int{2, 1}, []int{3, 3}}, []int{4, 3})) // [3, 4]
