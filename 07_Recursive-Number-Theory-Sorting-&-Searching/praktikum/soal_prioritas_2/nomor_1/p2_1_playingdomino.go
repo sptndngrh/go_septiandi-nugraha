95803839bf88b8dfff93f56611af3849240d76b3
Septiandi Nugraha <septiandin92@gmail.com>
2023-08-31 20:16:35 +0700

    modify code soal prioritas 2 materi 07

--- 07_Recursive-Number-Theory-Sorting-&-Searching/praktikum/soal_prioritas_2/nomor_1/p2_1_playingdomino.go
+++ 07_Recursive-Number-Theory-Sorting-&-Searching/praktikum/soal_prioritas_2/nomor_1/p2_1_playingdomino.go
@@ -17,7 +17,7 @@ func playingDomino(cards [][]int, deck []int) interface{} {
 }
 
 func main() {
-	fmt.Println(playingDomino([][]int{[]int{6, 5}, []int{3, 4}, []int{2, 1}, []int{3, 3}}, []int{4, 3})) // [3, 4]
-	fmt.Println(playingDomino([][]int{[]int{6, 5}, []int{3, 3}, []int{3, 4}, []int{2, 1}}, []int{3, 6})) // [6, 5]
-	fmt.Println(playingDomino([][]int{[]int{6, 6}, []int{2, 4}, []int{3, 6}}, []int{5, 1})) // "Tutup kartu"
+	fmt.Println(playingDomino([][]int{{6, 5}, {3, 4}, {2, 1}, {3, 3}}, []int{4, 3})) // [3, 4]
+	fmt.Println(playingDomino([][]int{{6, 5}, {3, 3}, {3, 4}, {2, 1}}, []int{3, 6})) // [6, 5]
+	fmt.Println(playingDomino([][]int{{6, 6}, {2, 4}, {3, 6}}, []int{5, 1})) // "Tutup kartu"
 }
