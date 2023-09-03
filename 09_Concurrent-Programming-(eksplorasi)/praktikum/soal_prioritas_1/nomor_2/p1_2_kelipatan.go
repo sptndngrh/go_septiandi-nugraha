d71b42fefdd58d3b928ddad3bde4a0ad65fa2077
Angga Aditya <anggaaditia447@gmail.com>
2023-09-03 12:13:15 +0700

    Mengerjakan soal materi 09

---
+++ 09_Concurrent-Programming-(eksplorasi)/praktikum/soal_prioritas_1/nomor_2/p1_2_kelipatan.go
@@ -0,0 +1,40 @@
+// Soal 2
+// Buatlah program yang mencetak bilangan kelipatan 3 dengan menerapkan goroutine dan channel!
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func printMultipleThree(c chan int) {
+	for i := 1; ; i++ {
+		if i%3 == 0 {
+			c <- i
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	c := make(chan int) 
+
+	go printMultipleThree(c)
+
+	const sleepDurationSeconds = 10
+	timer := time.NewTimer(sleepDurationSeconds * time.Second)
+
+	for {
+		select {
+		case multiple := <- c:
+			fmt.Printf("%d merupakan kelipatan dari 3\n", multiple)
+		
+		case <-timer.C:
+			fmt.Println("Program berhenti setelah 10 detik")
+			return
+		}
+	}
+
+}
